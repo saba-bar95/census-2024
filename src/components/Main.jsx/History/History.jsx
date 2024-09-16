256bb5b17027c1c4a92c68ad6289103c67afe0ec
saba-bar95 <s.barbakadze95@gmail.com>
2024-09-16 16:25:33 +0400

    Add years container down history section

--- src/components/Main.jsx/History/History.jsx
+++ src/components/Main.jsx/History/History.jsx
@@ -27,6 +27,79 @@ export default function History() {
             <img src={map} alt="map" />
           </div>
         </div>
+        <div className="years-container">
+          <div className="years-child">
+            <p
+              className="dot"
+              style={{ fontSize: "1.5em", color: "rgba(247, 138, 35, 1)" }}
+            >
+              •
+            </p>
+            <div
+              className="border-container"
+              style={{ borderBottom: "3px solid rgba(247, 138, 35, 1)" }}
+            ></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">2014</p>
+            </div>
+          </div>
+          <div className="years-child">
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">2002</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1989</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1970</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1959</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1939</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1926</p>
+            </div>
+          </div>
+          <div className="years-child">
+            {" "}
+            <div className="border-container"></div>
+            <div className="text">
+              <p className="dot">•</p>
+              <p className="year">1897</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
