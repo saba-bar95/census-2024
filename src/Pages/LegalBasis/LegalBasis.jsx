d1998432548acbbca19038d4018b222c1272bfb8
saba-bar95 <s.barbakadze95@gmail.com>
2024-11-18 15:05:14 +0400

    change some links

--- src/Pages/LegalBasis/LegalBasis.jsx
+++ src/Pages/LegalBasis/LegalBasis.jsx
@@ -28,8 +28,8 @@ export default function LegalBasis() {
 
   const thirdLink =
     language === "en"
-      ? "https://matsne.gov.ge/en/document/view/1561437?publication=23"
-      : "https://matsne.gov.ge/ka/document/view/1561437?publication=33";
+      ? "https://matsne.gov.ge/en/document/view/5827307?publication=2"
+      : "https://matsne.gov.ge/ka/document/view/5827307?publication=2";
 
   const fourthLink =
     "https://www.geostat.ge/media/61668/%E1%83%A1%E1%83%90%E1%83%A5%E1%83%90%E1%83%A0%E1%83%97%E1%83%95%E1%83%94%E1%83%9A%E1%83%9D%E1%83%A1-2024-%E1%83%AC%E1%83%9A%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%9D%E1%83%A1%E1%83%90%E1%83%AE%E1%83%9A%E1%83%94%E1%83%9D%E1%83%91%E1%83%98%E1%83%A1-%E1%83%93%E1%83%90-%E1%83%A1%E1%83%90%E1%83%A1%E1%83%9D%E1%83%A4%E1%83%9A%E1%83%9D-%E1%83%A1%E1%83%90%E1%83%9B%E1%83%94%E1%83%A3%E1%83%A0%E1%83%9C%E1%83%94%E1%83%9D-%E1%83%90%E1%83%A6%E1%83%AC%E1%83%94%E1%83%A0%E1%83%98%E1%83%A1-%E1%83%9E%E1%83%A0%E1%83%9D%E1%83%92%E1%83%A0%E1%83%90%E1%83%9B%E1%83%90.pdf";
