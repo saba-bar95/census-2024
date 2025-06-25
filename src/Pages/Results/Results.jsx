e0e2f0cfd1c16031ad363ec61fccca94755ecc4a
saba-bar95 <s.barbakadze95@gmail.com>
2025-06-25 11:58:39 +0400

    add other result files

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -25,21 +25,42 @@ export default function Results() {
     },
   };
 
-  console.log(language);
+  const createDownloadHandler = (fileName) => {
+    const link = document.createElement("a");
+    link.href = `/${encodeURIComponent(fileName)}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-  const handleDownload = () => {
+  const handleDownloadFirst = () => {
     const fileName =
       language === "ka"
         ? "საქართველოს 2024 წლის მოსახლეობის და სასოფლო-სამეურნეო აღწერის წინასწარი შედეგები.pdf"
         : "2024 Population and Agricultural Census of Georgia Preliminary results.pdf";
+    createDownloadHandler(fileName);
+  };
 
-    const link = document.createElement("a");
-    link.href = `/${encodeURIComponent(fileName)}`;
-    link.download = fileName;
+  const handleDownloadSecond = () => {
+    const fileName =
+      language === "ka"
+        ? "01-mosaxleoba-RegionebiT-TviTmarTveli-erTeulebiT-da-saqalaqo-sasoflo-dasaxlebebiT.xlsx"
+        : "01-Population-by-regions-self-governed-units-and-urban-rural-settlements.xlsx";
+    createDownloadHandler(fileName);
+  };
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const handleDownloadThird = () => {
+    const fileName =
+      language === "ka"
+        ? "02-mosaxleoba-RegionebiT-TviTmarTveli-erTeulebiT-da-sqesiT.xlsx"
+        : "02-Population-by-regions-self-governed-units-and-sex.xlsx";
+    createDownloadHandler(fileName);
+  };
+
+  const handleDownloadFourth = () => {
+    const fileName = "Census 2024_25.06.2024.pdf";
+    createDownloadHandler(fileName);
   };
 
   return (
@@ -50,9 +71,14 @@ export default function Results() {
           <h1>{text.header}</h1>
           <div className="links">
             <ul>
-              <li onClick={handleDownload}>{links[language].first}</li>
-              <li>{links[language].second}</li>
-              <li>{links[language].third}</li>
+              <li onClick={handleDownloadFirst}>{links[language].first}</li>
+              <li onClick={handleDownloadSecond}>{links[language].second}</li>
+              <li onClick={handleDownloadThird}>{links[language].third}</li>
+              {language === "ka" && (
+                <li onClick={handleDownloadFourth}>
+                  2024 წლის მოსახლეობის აღწერის წინასწარი შედეგები
+                </li>
+              )}
             </ul>
           </div>
         </div>
